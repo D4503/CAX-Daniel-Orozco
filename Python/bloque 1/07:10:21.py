7f0bcb7eecd949ebc2a3c420408aee51d4ee6a7c
Daniel Orozco <104459459+D4503@users.noreply.github.com>
2022-05-17 13:50:22 -0500

    Add files via upload

---
+++ Python/bloque 1/07:10:21.py
@@ -0,0 +1,15 @@
+print('aprobados')
+print('5')
+
+
+mensaje='aprobados'
+mensaje2='reprobados'
+
+print(mensaje)
+print(mensaje2)
+print(mensaje)
+print(mensaje2)
+
+mensaje3='hello world my name is Daniel Orozco, I am in 9°A grade, I really like the ropa vieja that my grandma makes me, and I really like to drive'
+
+print(mensaje3)
