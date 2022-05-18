5b7d166bb9c1ff0bebb6a3e5801b5b27edf996a1
Daniel Orozco <104459459+D4503@users.noreply.github.com>
2022-05-18 13:38:46 -0500

    Codigo sin errores 18/05/22
    Si pasa la fecha del 18/06/22 ya no lo uses, usa uno mas reciente (para Daniel del futuro)

---
+++ Python/Bloque 3/Pr0yecto.py
@@ -0,0 +1,231 @@
+esmeraldas=0
+sdl=0
+xp=0
+
+
+print("ºººººººººººººººººººººººº")
+print("                º       ")
+print("       ºº      ººº      ")
+print("    ºººº      ººººº     ")
+print("    º º         º       ")
+print("ºººººººººººººººººººººººº")
+print("este eres tu, Edward un pollo perdido en un bosque por alejarte de tu mamá")
+input("")
+print("no ves a tus amigos... ni ha tu familia crees que es tu fin...")
+input("")
+print("al verte en una situacion de peligro temes por lo que te puede pasar y huyes ")
+input("")
+print("te encuentras un sable de luz roto... te acuerdas de tu entrenamiento jedi con Shrek y tienes dos opciones")
+input("")
+print("tomas el sable y decides buscar algun objeto para repararlo y hacer tu propia aventura (1)")
+input("")
+print("o decides olvidarlo y alejarte a ver como le haces? (2)")
+print("checkpoint")
+print("respuesta en numero")
+while True:
+  ñ=int(input("prefieres la opcion 1 o 2? "))
+  
+  if (ñ==2):
+    print("intentaste sobrebivir pero te comio un T-rex vegetariano con cara de tortuga")
+    continue
+    
+  if (ñ==1):
+    print("y asi fue como emprendiste tu aventura...")
+    input("")
+    print("adentrandote en el bosque encontraste 3 caminos que llevan a distintos lugares")
+    input("")
+    print("el primero te lleva a un mini pekka")
+    input("")
+    print("el segundo te lleva con un aldeano de una aldea cubica")
+    input("")
+    print("el tercero te lleva con Salinas")
+    input("")
+    print("checkpoint")
+    print("respuesta en numero")
+    break
+  else:
+    print("si puedes leer o eres analfabeto")
+while True:
+  P=int(input("que camino eliges? el 1, 2 o 3? "))
+      
+  if (P==3):
+    print("rayos salinas activo su modo anticoppel y te lanzo un impactrueno")
+    input("")
+    print("wasted")
+    continue
+  elif (P==1):
+    print("el mini pekka anda en busca de la torre del rey no puede hablar contigo")
+    input("")
+    print("decides regresar a ver que hay en los otros caminos")
+    continue
+  elif (P==2):
+    print("ves el aldeano y decide tradear la pieza que te falta para tu sable Jedi a cambio de 3 esmeraldas")
+    input("")
+    print("decides buscar las esmeraldas y no sabes por donde empezar")
+    print("tienes dos opciones la primera es buscar en una montaña que ves a lo lejos")
+    input("")
+    print("la segunda es ir a preguntarle a los demas aldeanos como encontrarlas")
+    print("checkpoint")
+    print("respuesta en número")
+    break
+  else:
+    print("si puedes leer o eres analfabeto")
+while True:
+  u=int(input("que opcion decides tomar?"))
+  if (u==2):
+    print("un aldeano te dice que te tradea 1 esmeralda a cambio de que le traigas la cabeza de Salinas")
+    input("")
+    print("aceptas la tarea y vas a buscar a salinas") 
+    input("")
+    print("al ver que es demasiado poderoso decides huir, no estas preparado aun...")
+    continue
+  if (u==1):
+    print("te vas hacia la montaña pero mientras que caminas te das cuenta que te hace falta el equipo necesario para obtener esmeralda y escalar la montaña...")
+    input("")
+    print("decides volver y justamente un aldeano te tradea ambos objetos a cambio de tomar la vida de su mejor amigo")
+    input("")
+    print("tu al escuchar esto quedas sorprendido, nunca haz tomado la vida de alguien")
+    input("")
+    print("no sabes que hacer pero sabes que necesitas ese equipo")
+    input("")
+    print("cumpliras el reto de tomar la vida de su mejor amigo?")
+    print("checkpoint")
+    print("respuesta en número")
+    break
+  else:
+    print("si puedes leer o eres analfabeto")
+
+xp=xp+10
+    
+while True:
+  L=int(input("decides tomar la vida de su mejor amigo(1) o no tomarla?(2) "))
+  if (L==1):
+    print("ya te dijo donde vive su amigo y te planteas como matarlo")
+    input("")
+    print("hay una ventana que siempre esta abierta y decides ver si por ahi puedes entrar")
+    input("")
+    print("entras pero es el baño, huele a popo y decides esperar 30 minutos a que deje de oler")
+    print("una ves que dejo de oler entras y ves que esta dormido, tienes la oportunidad perfecta, pero ahora hay otro problema...")
+    input("")
+    print("no sabes con que matarlo y ves que no tienes ninguna herramienta util.")
+    input("")
+    print("te planteas lo que puedes hacer pero no se te ocurre nada, al final piensas ver que habitaciones hay donde encontrar algo para matarlo.") 
+    input("")
+    print("donde vas a querer buscar primero?")
+    print("puedes ir al dormitorio(1), a la cocina(2) o al cuarto de su abuelo(3)")
+    print("checkpoint")
+    print("respuesta en número")
+    break
+
+    
+  if (L==2):
+    print("al ver que no tienes el valor decides irte de la casa y te vas por uu extraño camino que hay en el bosque")
+    input("")
+    print("al caminar durante 6 horas pofin encuentras a salida del bosque yhay una extraña carretera.")
+    input("")
+    print("a lo lejos ves una silueta humana rosa, pero sientes un extraño cosquilleo")
+    input("")
+    print("al fijarte bien te das uenta que es tu antigua compañera en la academia de shrek...")
+    input("")
+    print("defintivamente es Barbie!")
+    input("")
+    print("pero no viene hacia ti de manera amigable...")
+    input("")
+    print("justo un momento antes de llegar a ti llega un camion volador impulsado por mr robertos y ves a alguien con casco e inmediatamente sabes que es darth vader")
+    input("")
+    print("sabes que algo no esta bien y ves una luz gigante del tamaño del sol y una tipo de luna, es la estrella de la muerte!! ")
+    input("")
+    print("ves a tu antiguo maestro...")
+    input("")
+    print("es shrek!!")
+    input("")
+    print("dato curioso sangre es verde")
+    input("")
+    print("esta practicamente moribundo")
+    input("")
+    print("en un intento por defenderte con su humildad infinita hace un escudo magico y recibe el daño de lleno...")
+    input("")
+    print("lograste ver por utima vez su sonrisa...")
+    input("")
+    print("el muere siendo desintegrado, el planeta fue explotado y tu al tener tanto odio terminaste siendo la persna ma poderosa del mundo aunque de tanto poder moriste por autodestruccion...")
+    input("")
+    print("Felicidades llegaste al primer final del juego...")
+    print("todos murieron, es el bad ending, pero toma 1 estrela")
+    print("logro desboqueado: primer final.")
+
+xp=xp+10
+while True:
+  n=int(input("a que cuarto vas?"))
+
+  if (n==1):
+    print("estas en el dormitorio y te das cuenta que en el cajon hay una pistola...")
+    input("")
+    print("decides utilizarla pero era tan oscuro que no te diste cuenta que era de balines, le disparas se despierta y sale huyendo.")
+    continue
+
+  if (n==2):
+    print("entras a la cocina y te das cuenta que es un desorden, no tiene platos lavados, tiene maruchans tiradas en el suelo, etc buscas un. cuchillo pero te das cuenta que no hay nada...")
+    input("")
+    print("con suerte viste un florero asi que decidiste tomarlo e intentar romperselo en la cabeza lo lograste, ya lo habias matado, pero no sabias que tenia vecinos y te escucharon")
+    input("")
+    print("al darse cuenta su vecino, la rana juan, viendote desde el balcon le marca a los perros policias, (son elefantes, pero es una rana, no entiendo que esperabas de una rana nombrando animales)")
+    input("")
+    print("los elefantes legan a ti y te esposan (logro desbloqueado por fin tienes 2 esposas, aunque no son para lo que buscas), te condenan a cadena perpetua por homicidio y al darte cuenta que su amigo tenia 17 años se convierte en homicidio infantil por lo que te condenan a muerte...")
+    continue
+  if (n==3):
+    print("cuarto de abuelo")
+    print("al entrar huele a cigarro y piensas el porque toda la casa huele mal, al final te encuentras una cajetilla de cigarros  un encendedor...")
+    input("")
+    print("te preguntas de que te servira un encendedor y una cajetilla de cigarros hasta que ves que la casa es de madera, la piensas prender pero en un cajon te encuentras 1 esmeralda...")
+    input("")
+    print("piensas que ya por fin nada mas te faltan 2 y que estas cerca de cumplir tu cometido hasta que decides incendiar la casa llevandote la esmeralda ")
+    input("a medio incendio ves como algo brilla por dentro de la casa y decides entrar a ver que es...")
+    input("")
+    print("hay tanto humo que apenas y puedes ver que es, al parecer escucha un grito y era justamente a quien buscabas matar te apuras a buscar que era lo que veias...")
+    input("")
+    print("al ver bien era un espejo y reflejaba el fuego, piensas que estas perdido pero justamente te das cuenta que hay algo mas y es de color azul")
+    input("")
+    print("al verlo bien es la parte que te falta del sable de luz")
+    input("")
+    print("sientes la presencia de la persona que estabas por matar. y en un abrir y cerrar de ojos habias reparado el sable")
+    input("")
+    print("recordando tu entrenamiento con shrek te haces tremendo 360 justo antes de que te llegue a tocar el objetivo y le cortas de un solo tajo invertido la cabeza sales corriendo de ahí")
+    input("")
+    print("regresas a la aldea por el equipo de mineria para asi poder irte lejos de ahi a vivir en las montañas viendo que ya no necesitas las esmeraldas")
+    input("al llegar a reclamar tu equipo de mineria te das cuenta que el aldeano te estafo y te denuncio, a parte te presume el equipo de mineria en tu cara")
+    input("")
+    print("claramente tu entrenamiento no fue en vano y decides enfrentarte a los policias...")
+    input("")
+    print("claramente te superan en numero es un 1 vs 8")
+    xp=xp+100
+    input("")
+    print("como sabes que es tu unica oportunidad de obtener el equipo y salir huyendo al principio no tuviste problemas lo manejaste todo muy bien, mantuviste la calma y recordaste todo sobre tu entrenamiento con shrek el problema fue el 8° policia")
+    input("")
+    print("ya que normalmente llevan 2 escuadrones y uno de ellos esta comandado por alguien de rango superior, este se esecializa en magia y combate lejano, es decir, todo lo contrario a ti")
+    input("")
+    print("sus ataques son muy rapidos y sabes que si te tardas en asesinarlo eres hombre muerto por los refuerzos, te planteas como vencerlo y llegas a 2 conclusiones")
+    input("")
+    print("la mas obvia seria intentar esquivar tantos proyectiles como te sea posible pero no sabes si eres capas y la segunda opcion es retroceder y tomar al aldeano como rehen")
+    print("checkpoint")
+    print("respuesta en numero")
+    break
+
+esmeraldas=esmeraldas+1
+sdl=sdl+1
+
+print("para este punto tienes", xp,"de experiencia")
+while True:
+  I=input("prefieres intentar acercarte a ver si tienes oportunidad(1) o retroceder un poco y tomar al aldeano como rehen (2)")
+  if (I==2):
+    print("esto hacia el error")
+  if (I==1):
+    print("esquivando la mayoria de ataques ya te encuentras a 8 metros, sabes que si te acercas 6 metros más lo alcanzas...")
+    input("")
+    print("el problema fue que lanzo una bola de fuego ")
+    input("")
+    print("al estar tan cerca a 4 metros de poder golpearlo y ver esa enorme bola de fuego ir tan rapido pensaste 2 soluciones")
+    input("")
+    print("podrias intentar partirla a la mitad con los daños que tienes, tomando en cuenta que es de fuego...")
+    input("")
+    print("o podrias saltar con lo que te queda de fuerza esquivando la bola pero quedando vulnerable en el aire")
+    break
